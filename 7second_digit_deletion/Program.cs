2bb763a0af86e875713191e40fa8ec585e013043
Yulia Shurygina <zavalishina-yu@mail.ru>
2021-12-21 14:53:40 +0300

    tens delitions

---
+++ 7second_digit_deletion/Program.cs
@@ -0,0 +1,7 @@
+﻿// Удалить вторую цифру трёхзначного числа
+int number = new Random().Next(100, 1000);
+Console.WriteLine("Трехзначное число " + number);
+int hundred = number / 100;
+int unit = number % 10; 
+
+Console.WriteLine("Удалили десятки " + hundred + unit);
