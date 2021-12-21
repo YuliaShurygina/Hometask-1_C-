e13710d911248c74f07c5d809063a2e810e2e1bd
Yulia Shurygina <zavalishina-yu@mail.ru>
2021-12-21 14:42:22 +0300

    Print maximum digit

---
+++ 6maximum_digit/Program.cs
@@ -0,0 +1,13 @@
+﻿// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+int number = new Random().Next(10, 100);
+Console.WriteLine("число " + number);
+int tens = number % 100 / 10; 
+int unit = number % 10; 
+if (tens > unit)
+{
+    Console.WriteLine("Наибольшая цифра " + tens);
+}
+else
+{
+    Console.WriteLine("Наибольшая цифра " + unit);
+}
