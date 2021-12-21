52f6ed307ea3244be4dda0c51b299cf73e148cb9
Yulia Shurygina <zavalishina-yu@mail.ru>
2021-12-21 15:14:40 +0300

    remainder definition

---
+++ 8kratnoe_chislo/Program.cs
@@ -0,0 +1,18 @@
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток.
+int number1 = new Random().Next(1, 1000);
+Console.WriteLine("Заданное число " + number1);
+Console.Write("Введите число  ");
+int number2 = int.Parse(Console.ReadLine()??"0");
+int remainder = number1 % number2;
+if (remainder == 0)
+{
+    Console.WriteLine("Число кратно заданному");
+}
+else
+{
+    Console.WriteLine("Остаток от деления равен " + remainder);
+}
+
+
+
+
