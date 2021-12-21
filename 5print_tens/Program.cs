291e04e4fd563dd5c651f144248fe6e9f28a47c6
Yulia Shurygina <zavalishina-yu@mail.ru>
2021-12-21 14:22:32 +0300

    Print tens

---
+++ 5print_tens/Program.cs
@@ -0,0 +1,5 @@
+﻿// Показать вторую цифру трёхзначного числа
+Console.WriteLine("Введите трехзначное число : ");
+int number = int.Parse(Console.ReadLine()??"0");
+int tens = number % 100 / 10; 
+Console.WriteLine("Вторая цифра "+tens);
