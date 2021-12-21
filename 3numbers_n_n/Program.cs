43b8501aabc0255ef832dd2d621e412a3907c519
Yulia Shurygina <zavalishina-yu@mail.ru>
2021-12-21 13:48:38 +0300

    Numbers from -N to N

---
+++ 3numbers_n_n/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число N: ");
+int number = int.Parse(Console.ReadLine()??"0");
+int i = 0;
+
+while (i <= number*2)
+{
+    int k = i - number;
+    Console.Write(k +" " );   
+    i = i + 1;
+}
+
