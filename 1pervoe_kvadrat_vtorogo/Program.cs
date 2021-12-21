d29f4fdcb48215e54bcc79579a71f0b84550ce0a
Yulia Shurygina <zavalishina-yu@mail.ru>
2021-12-21 12:58:21 +0300

    if first number is square of second

---
+++ 1pervoe_kvadrat_vtorogo/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите число  ");
+int numberA = int.Parse(Console.ReadLine()??"0");
+Console.Write("Введите число  ");
+int numberB = int.Parse(Console.ReadLine()??"0");
+
+if (numberA /numberB  == numberB)
+{
+    Console.WriteLine("Да, первое число является квадратом второго");
+}
+else
+{
+    Console.WriteLine("Нет, первое число не является квадратом второго");
+}
+
+
