d02b2c0fe3c2bcbfcfd35b317620a6cb78489876
Yulia Shurygina <zavalishina-yu@mail.ru>
2021-12-21 17:28:11 +0300

    third numeral from the begining of the number

---
+++ 9third_digit/Program.cs
@@ -0,0 +1,20 @@
+﻿// Найти третью цифру числа или сообщить, что её нет
+Console.WriteLine("Введите число : ");
+int number = int.Parse(Console.ReadLine()??"0");
+int unit = number % 10; 
+if (number < 100) 
+{
+    Console.WriteLine("Третьей цифры числа нет" );
+}
+
+if (number <= 999 & number > 99)
+{
+        Console.WriteLine("Третья цифра числа = " + unit);
+}
+while (number > 999)
+{
+     number = number / 10;
+    
+}
+unit = number % 10;
+Console.WriteLine("Третья цифра числа = " + unit);
